e54ee509e407415d1daf37faae25a4ee35eda753
maurice diong <mdiong00@gmail.com>
2023-12-02 04:02:14 -0500

    context lab completed.

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    const employeeRecord = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return employeeRecord;
+
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    const employeeRecords = arrayOfArrays.map(createEmployeeRecord);
+    return employeeRecords;
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    //const obj = this
+    this.timeInEvents.push({
+        type:"TimeIn",
+        hour:parseInt(hour),
+        date:date
+        }
+    )
+    return this//obj
+}
+
+function createTimeOutEvent (dateStamp) {
+    const [date, hour] =dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type:"TimeOut",
+        hour:parseInt(hour),
+        date:date
+    }
+    )
+    return this;
+}
+
+function hoursWorkedOnDate(dateStamp) {
+       const timeCard = this.timeInEvents;
+       for (const num in timeCard) {
+        const dateWorked = timeCard[num]["date"]
+        if (dateStamp == dateWorked) {
+            const hoursWorked = 0.01*((this.timeOutEvents[num]['hour']) - (this.timeInEvents[num]['hour']));
+            return hoursWorked;
+        }
+    }
+}
+
+function wagesEarnedOnDate (dateStamp) {
+    const wages = hoursWorkedOnDate.call(this,dateStamp)*this.payPerHour;
+    return wages
+}
+//   const eventObj = this.timeInEvents.find(isDate)
+  //      function isDate (obj) {
+   //         return obj.date == dateStamp
+    //      const hoursWorked = eventObj.
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +81,24 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName (srcArray,firstName) {
+    const fname = firstName;
+    function isName (records) {
+        return records.firstName == fname
+    }
+    return srcArray.find(isName)
+}
+
+function sumFunc (total, num) {
+    return total + num
+}
+function calculatePayroll (array) {
+    const employeePay = [];
+    for (const records of array ) {
+       employeePay.push(allWagesFor.call(records));
+      // employeePay.push(allWages);
+    }
+    const totalPay = employeePay.reduce(sumFunc,0)
+    return totalPay
+
+}
